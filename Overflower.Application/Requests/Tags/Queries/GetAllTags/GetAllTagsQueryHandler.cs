e91fdf122cc2c38246afc03c73c9fa6f66083cdb
Katarzyna <kasiakadziolka@gmail.com>
2024-03-27 11:20:40 +0100

    Add migration

--- Overflower.Application/Requests/Tags/Queries/GetAllTags/GetAllTagsQueryHandler.cs
+++ Overflower.Application/Requests/Tags/Queries/GetAllTags/GetAllTagsQueryHandler.cs
@@ -11,6 +11,7 @@ public class GetAllTagsQueryHandler : IRequestHandler<GetAllTagsQuery, TagDto[]>
         _context = context;
     }
     public async Task<TagDto[]> Handle(GetAllTagsQuery request, CancellationToken cancellationToken) {
+        //tu chce pobrać 1000 tagów zze stackobverfloww jeśli nie ma i wrzucić do bazki i to pewnie będzie srwis
         var tags = await _context.Tags.ToArrayAsync(cancellationToken);
         return tags.Select(t => t.ToDto()).ToArray();
     }
---
+++ Overflower.Persistence/Migrations/20240327102007_AddTags.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Overflower.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Count = table.Column<int>(type: "integer", nullable: false),
+                    IsRequired = table.Column<bool>(type: "boolean", nullable: false),
+                    IsModeratorOnly = table.Column<bool>(type: "boolean", nullable: false),
+                    HasSynonyms = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Name);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
