612ab2a9f51776528f269ad9ceb9cc8a16ade226
Katarzyna <kasiakadziolka@gmail.com>
2024-04-02 14:48:58 +0200

    Mapper tests

--- Overflower.Application/Requests/Tags/TagMapper.cs
+++ Overflower.Application/Requests/Tags/TagMapper.cs
@@ -4,6 +4,8 @@ namespace Overflower.Application.Requests.Tags;
 
 public static class TagMapper {
     public static TagDto ToDto(this TagEntity tag, long totalCount) {
+        double percentageShare = 0;
+        if (totalCount != 0) percentageShare = (double)tag.Count / totalCount * 100;
         return new TagDto {
             Id = tag.Id,
             Name = tag.Name,
@@ -11,7 +13,7 @@ public static class TagMapper {
             HasSynonyms = tag.HasSynonyms,
             IsRequired = tag.IsRequired,
             IsModeratorOnly = tag.IsModeratorOnly,
-            PercentageShare = (double) tag.Count / totalCount * 100
+            PercentageShare = percentageShare
         };
     }
 }
---
+++ Overflower.UnitTests/Requests/Tags/TagMapperTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Overflower.Application.Requests.Tags;
+using Overflower.Persistence.Entities.Tags;
+using Overflower.Tests.Shared.Generators;
+
+namespace Overflower.UnitTests.Requests.Tags;
+
+public class TagMapperTests {
+    [Test]
+    [TestCase(10)]
+    [TestCase(100)]
+    [TestCase(1000)]
+    public void ToDto_ShouldReturnTagDto(long totalCount) {
+        // Arrange
+        TagEntity tagEntity = new TagGenerator().Generate();
+        var expectedPercentageCount = (double)tagEntity.Count / totalCount * 100;
+        // Act
+        var tagDto = tagEntity.ToDto(totalCount);
+        // Assert
+        tagDto.Id.Should().Be(tagEntity.Id);
+        tagDto.Name.Should().Be(tagEntity.Name);
+        tagDto.Count.Should().Be(tagEntity.Count);
+        tagDto.HasSynonyms.Should().Be(tagEntity.HasSynonyms);
+        tagDto.IsRequired.Should().Be(tagEntity.IsRequired);
+        tagDto.IsModeratorOnly.Should().Be(tagEntity.IsModeratorOnly);
+        tagDto.PercentageShare.Should().Be(expectedPercentageCount);
+    }
+    
+    [Test]
+    public void ToDto_TotalCountEqualZero_ShouldReturnTagDtoWithPercentageShareEqualZero() {
+        // Arrange
+        TagEntity tagEntity = new TagGenerator().Generate();
+        var expectedPercentageCount = 0;
+        // Act
+        var tagDto = tagEntity.ToDto(0);
+        // Assert
+        tagDto.Id.Should().Be(tagEntity.Id);
+        tagDto.Name.Should().Be(tagEntity.Name);
+        tagDto.Count.Should().Be(tagEntity.Count);
+        tagDto.HasSynonyms.Should().Be(tagEntity.HasSynonyms);
+        tagDto.IsRequired.Should().Be(tagEntity.IsRequired);
+        tagDto.IsModeratorOnly.Should().Be(tagEntity.IsModeratorOnly);
+        tagDto.PercentageShare.Should().Be(expectedPercentageCount);
+    }
+}
