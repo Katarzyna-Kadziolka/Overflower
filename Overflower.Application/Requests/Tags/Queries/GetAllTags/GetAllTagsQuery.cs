7a65110dce0ce0afba66a1b0995e0baa27a9921c
Katarzyna <kasiakadziolka@gmail.com>
2024-04-02 15:26:48 +0200

    Docker

--- Overflower.Application/Requests/Tags/Queries/GetAllTags/GetAllTagsQuery.cs
+++ Overflower.Application/Requests/Tags/Queries/GetAllTags/GetAllTagsQuery.cs
@@ -4,8 +4,8 @@ using Overflower.Application.Paging;
 namespace Overflower.Application.Requests.Tags.Queries.GetAllTags;
 
 public class GetAllTagsQuery : IRequest<PageResult<TagDto>>, IPaging {
-    public required int Page { get; set; }
-    public required int PageSize { get; set; }
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 50;
     public TagSortBy TagSortBy { get; set; } = TagSortBy.None;
     public SortingOrder SortingOrder { get; set; } = SortingOrder.Ascending;
 }
